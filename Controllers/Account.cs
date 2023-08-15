16db6de979b7a94238f80e6af499ad499169c9f8
Alex <giraldojhong4@gmail.com>
2023-08-15 12:04:27 -0500

    Metodo find all en Account

--- Controllers/Account.cs
+++ Controllers/Account.cs
@@ -92,6 +92,56 @@ public static class Account
 
 
 
+
+
+    /// <summary>
+    /// Obtiene los datos de una cuenta especifica
+    /// </summary>
+    /// <param name="id">ID de la cuenta</param>
+    public async static Task<ReadAllResponse<AccountModel>> Read(List<int> id)
+    {
+
+        // Crear HttpClient
+        using var httpClient = new HttpClient();
+
+        // ApiServer de la solicitud GET
+        string url = ApiServer.PathURL("account/find");
+
+
+        var json = JsonConvert.SerializeObject(id);
+
+        // Crear HttpRequestMessage y agregar el encabezado
+        var request = new StringContent(json, Encoding.UTF8, "application/json");
+
+
+
+        try
+        {
+
+            // Hacer la solicitud GET
+            HttpResponseMessage response = await httpClient.PostAsync(url, request);
+
+            // Leer la respuesta como una cadena
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+
+            var obj = JsonConvert.DeserializeObject<ReadAllResponse<AccountModel>>(responseBody);
+
+            return obj ?? new();
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error al hacer la solicitud GET: {e.Message}");
+        }
+
+
+        return new();
+    }
+
+
+
     /// <summary>
     /// Obtiene los datos de una cuenta
     /// </summary>
