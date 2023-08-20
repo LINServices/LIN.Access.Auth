aa2080e7942247edc8daf81912de227a9182b20f
Alex <giraldojhong4@gmail.com>
2023-08-19 22:41:52 -0500

    Conexion con la api de Auth Passkey

---
+++ Controllers/Intents.cs
@@ -0,0 +1,50 @@
+﻿namespace LIN.Access.Auth.Controllers;
+
+
+public static class Intents
+{
+
+
+    /// <summary>
+    /// Obtiene los intentos de Passkey que no han sido aceptados
+    /// </summary>
+    public async static Task<ReadAllResponse<PassKeyModel>> ReadAll(string token)
+    {
+
+        // Crear HttpClient
+        using var httpClient = new HttpClient();
+
+        // ApiServer de la solicitud GET
+        string url = ApiServer.PathURL("intents");
+
+        // Crear HttpRequestMessage y agregar el encabezado
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+        request.Headers.Add("token", $"{token}");
+
+        try
+        {
+
+            // Hacer la solicitud GET
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            // Leer la respuesta como una cadena
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+
+            var obj = JsonConvert.DeserializeObject<ReadAllResponse<PassKeyModel>>(responseBody);
+
+            return obj ?? new();
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error al hacer la solicitud GET: {e.Message}");
+        }
+
+
+        return new();
+    }
+
+
+}
