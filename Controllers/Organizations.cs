6f52285d90d7f39db5c4636c4f4069d06762eb3b
Alex <giraldojhong4@gmail.com>
2023-08-21 09:55:58 -0500

    Controlador de Orgs

---
+++ Controllers/Organizations.cs
@@ -0,0 +1,52 @@
+﻿namespace LIN.Access.Auth.Controllers;
+
+
+public static class Organizations
+{
+
+
+    /// <summary>
+    /// Obtiene los datos de una cuenta especifica
+    /// </summary>
+    /// <param name="id">ID de la cuenta</param>
+    public async static Task<ReadAllResponse<AccountModel>> ReadMembers(string token)
+    {
+
+        // Crear HttpClient
+        using var httpClient = new HttpClient();
+
+
+        httpClient.DefaultRequestHeaders.Add("token", $"{token}");
+        // ApiServer de la solicitud GET
+        string url = ApiServer.PathURL("org/members");
+
+
+        try
+        {
+
+            // Hacer la solicitud GET
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+
+            // Leer la respuesta como una cadena
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+
+            var obj = JsonConvert.DeserializeObject<ReadAllResponse<AccountModel>>(responseBody);
+
+            return obj ?? new();
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error al hacer la solicitud GET: {e.Message}");
+        }
+
+
+        return new();
+    }
+
+
+
+
+}
