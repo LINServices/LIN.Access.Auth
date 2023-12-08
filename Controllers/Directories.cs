5bf074ef067c8beff1b55990da7da5b178f34984
Alex <giraldojhong4@gmail.com>
2023-12-08 17:19:18 -0500

    Correccion de rutas en Directory

---
+++ Controllers/Directories.cs
@@ -0,0 +1,33 @@
+﻿namespace LIN.Access.Auth.Controllers;
+
+
+public class Directories
+{
+
+
+    /// <summary>
+    /// Obtener un directorio.
+    /// </summary>
+    /// <param name="id">Id del directorio.</param>
+    /// <param name="token">Token de acceso.</param>
+    public static async Task<ReadOneResponse<DirectoryModel>> Read(int id, string token)
+    {
+
+        // Cliente.
+        Client client = Service.GetClient("applications");
+
+        client.AddParameter("id", id.ToString());
+
+        // Headers.
+        client.DefaultRequestHeaders.Add("token", token);
+
+        // Respuesta.
+        var response = await client.Get<ReadOneResponse<DirectoryModel>>();
+
+        return response;
+
+    }
+
+
+
+}
