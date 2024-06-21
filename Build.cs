007fce7591ed8b0265e315ea44a91b796c75eb2f
Alex <giraldojhong4@gmail.com>
2024-06-21 06:19:09 -0500

    Nuevo dominio

--- Build.cs
+++ Build.cs
@@ -15,7 +15,7 @@ public class Build
     public static void Init(string? url = null)
     {
         Service._Service = new();
-        Service._Service.SetDefault(url ?? "https://api.identity.linapps.co/");
+        Service._Service.SetDefault(url ?? "https://api.identity.linplatform.com/");
     }
 
 }
