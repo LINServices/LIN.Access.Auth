6545c739febe1e666cbfb661e22c9f73dcdc7e12
Alexander Giraldo <giraldojhong4@gmail.com>
2024-10-14 15:53:51 -0500

    Mejoras

--- Controllers/Policies.cs
+++ Controllers/Policies.cs
@@ -1,80 +1,96 @@
 ﻿namespace LIN.Access.Auth.Controllers;
 
 
-//public class Policies
-//{
+public class Policies
+{
 
 
-//    /// <summary>
-//    /// Crear política en un directorio.
-//    /// </summary>
-//    /// <param name="policy">Modelo</param>
-//    /// <param name="token">Token</param>
-//    public static async Task<CreateResponse> Create(PolicyModel policy, string token)
-//    {
+    /// <summary>
+    /// Crear política en un directorio.
+    /// </summary>
+    /// <param name="policy">Modelo</param>
+    /// <param name="token">Token</param>
+    public static async Task<CreateResponse> Create(PolicyModel policy, string token, int? organization = null, bool assign = false)
+    {
 
-//        // Cliente.
-//        Client client = Service.GetClient("policies");
+        // Cliente.
+        Client client = Service.GetClient("policies");
 
-//        // Headers.
-//        client.AddHeader("token", token);
+        // Headers.
+        client.AddHeader("token", token);
+        client.AddHeader("assign", assign);
+        if (organization is not null)
+            client.AddHeader("organization", organization.Value);
 
-//        // Respuesta
-//        var response = await client.Post<CreateResponse>(policy);
+        // Respuesta
+        var response = await client.Post<CreateResponse>(policy);
 
-//        return response;
+        return response;
 
-//    }
-
-
-
-//    /// <summary>
-//    /// Valida el acceso a un permiso (Política de permisos) de una identidad.
-//    /// </summary>
-//    /// <param name="identity">Id de la identidad.</param>
-//    /// <param name="policy">Id de la política</param>
-//    public static async Task<ReadOneResponse<bool>> Validate(int identity, int policy)
-//    {
-
-//        // Cliente.
-//        Client client = Service.GetClient("policies/access");
-
-//        // Headers.
-//        client.AddParameter("identity", identity.ToString());
-//        client.AddParameter("policy", policy.ToString());
-
-//        // Respuesta
-//        var response = await client.Get<ReadOneResponse<bool>>();
-
-//        return response;
-
-//    }
+    }
 
 
 
-//    /// <summary>
-//    /// Obtiene las políticas asociadas a un directorio.
-//    /// </summary>
-//    /// <param name="token">Token de acceso.</param>
-//    /// <param name="directory">Id del directorio.</param>
-//    public static async Task<ReadAllResponse<PolicyModel>> ReadAll(int directory,string token)
-//    {
+    ///// <summary>
+    ///// Valida el acceso a un permiso (Política de permisos) de una identidad.
+    ///// </summary>
+    ///// <param name="identity">Id de la identidad.</param>
+    ///// <param name="policy">Id de la política</param>
+    //public static async Task<ReadOneResponse<bool>> Validate(int identity, int policy)
+    //{
 
-//        // Cliente.
-//        Client client = Service.GetClient("policies/read/all");
+    //    // Cliente.
+    //    Client client = Service.GetClient("policies/access");
 
-//        // Headers.
-//        client.AddHeader("token", token);
+    //    // Headers.
+    //    client.AddParameter("identity", identity.ToString());
+    //    client.AddParameter("policy", policy.ToString());
 
-//        // Params.
-//        client.AddParameter("directory", directory.ToString());
+    //    // Respuesta
+    //    var response = await client.Get<ReadOneResponse<bool>>();
 
-//        // Respuesta
-//        var response = await client.Get<ReadAllResponse<PolicyModel>>();
+    //    return response;
 
-//        return response;
-
-//    }
+    //}
 
 
-//}
+
+
+    public static async Task<ReadAllResponse<PolicyModel>> ReadAll(int organization, string token)
+    {
+
+        // Cliente.
+        Client client = Service.GetClient("policies/organization/all");
+
+        // Headers.
+        client.AddHeader("token", token);
+        client.AddHeader("organization", organization);
+
+        // Respuesta
+        var response = await client.Get<ReadAllResponse<PolicyModel>>();
+
+        return response;
+
+    }
+
+
+
+    public static async Task<ReadAllResponse<PolicyModel>> Aplicable(int identity, string token)
+    {
+
+        // Cliente.
+        Client client = Service.GetClient("policies/complacent/applicable");
+
+        // Headers.
+        client.AddHeader("token", token);
+        client.AddHeader("identity", identity);
+
+        // Respuesta
+        var response = await client.Get<ReadAllResponse<PolicyModel>>();
+
+        return response;
+
+    }
+
+
+}
